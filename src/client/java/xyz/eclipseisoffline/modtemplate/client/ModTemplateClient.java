a53236254fca4dc38cdfd0e970ff57d595e49178
Eclipse <eclipse@eclipseisoffline.xyz>
2025-10-02 15:47:10 +0000

    Start work on mod template

---
+++ src/client/java/xyz/eclipseisoffline/modtemplate/client/ModTemplateClient.java
@@ -0,0 +1,10 @@
+package xyz.eclipseisoffline.modtemplate.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTemplateClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/xyz/eclipseisoffline/modtemplate/ModTemplate.java
@@ -0,0 +1,10 @@
+package xyz.eclipseisoffline.modtemplate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModTemplate implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
