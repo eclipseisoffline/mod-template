b17723bd14db61f214d6b026aeee607e5f06944f
Eclipse <eclipse@eclipseisoffline.xyz>
2025-12-28 10:50:11 +0000

    Switch to official MultiMod

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,18 @@
 pluginManagement {
     repositories {
-        mavenLocal()
         mavenCentral()
         gradlePluginPortal()
+
+        maven {
+            name = "eclipseisoffline"
+            url = uri("https://maven.eclipseisoffline.xyz/releases")
+        }
+
         maven {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
         }
+
         maven {
             name = "NeoForged"
             url = uri("https://maven.neoforged.net/releases")
